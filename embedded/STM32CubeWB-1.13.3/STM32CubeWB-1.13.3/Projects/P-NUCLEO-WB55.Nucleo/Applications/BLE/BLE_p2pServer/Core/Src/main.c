e30fbff61c08b87167c5845ff1901f3abd247c2f
JJ Oswald <jjo50@pitt.edu>
2024-02-19 00:34:00 -0500

    sd card + ble code running + cleanup

--- embedded/STM32CubeWB-1.13.3/STM32CubeWB-1.13.3/Projects/P-NUCLEO-WB55.Nucleo/Applications/BLE/BLE_p2pServer/Core/Src/main.c
+++ embedded/STM32CubeWB-1.13.3/STM32CubeWB-1.13.3/Projects/P-NUCLEO-WB55.Nucleo/Applications/BLE/BLE_p2pServer/Core/Src/main.c
@@ -45,6 +45,10 @@
 /* USER CODE BEGIN Includes */
 #include "app_fatfs.h"
 #include "dbg_trace.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h> //for va_list var arg functions
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -138,18 +142,101 @@ int main(void)
   MX_RF_Init();
   MX_RTC_Init();
   MX_RNG_Init();
+
+  // TODO: cleanup, I moved this up
+  MX_APPE_Init();
+
   /* USER CODE BEGIN 2 */
   MX_SPI2_Init();
   if (MX_FATFS_Init() != APP_OK) {
     Error_Handler();
   }
 
-
   PRINT_MESG_DBG("Hi this is JJ \n");
+
+  PRINT_MESG_DBG("\r\n~ SD card demo by kiwih ~\r\n\r\n");
+
+    HAL_Delay(1000); //a short delay is important to let the SD card settle
+
+    //some variables for FatFs
+    FATFS FatFs; 	//Fatfs handle
+    FIL fil; 		//File handle
+    FRESULT fres; //Result after operations
+
+    //Open the file system
+    fres = f_mount(&FatFs, "", 1); //1=mount now
+    if (fres != FR_OK) {
+  	PRINT_MESG_DBG("f_mount error (%i)\r\n", fres);
+  	while(1);
+    }
+
+    //Let's get some statistics from the SD card
+    DWORD free_clusters, free_sectors, total_sectors;
+
+    FATFS* getFreeFs;
+
+    fres = f_getfree("", &free_clusters, &getFreeFs);
+    if (fres != FR_OK) {
+  	PRINT_MESG_DBG("f_getfree error (%i)\r\n", fres);
+  	while(1);
+    }
+
+    //Formula comes from ChaN's documentation
+    total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
+    free_sectors = free_clusters * getFreeFs->csize;
+
+    PRINT_MESG_DBG("SD card stats:\r\n%10lu KiB total drive space.\r\n%10lu KiB available.\r\n", total_sectors / 2, free_sectors / 2);
+
+    //Now let's try to open file "test.txt"
+    fres = f_open(&fil, "TEST.txt", FA_READ);
+    if (fres != FR_OK) {
+  	PRINT_MESG_DBG("f_open error (%i)\r\n");
+  	while(1);
+    }
+    PRINT_MESG_DBG("I was able to open 'test.txt' for reading!\r\n");
+
+    //Read 30 bytes from "test.txt" on the SD card
+    BYTE readBuf[30];
+
+    //We can either use f_read OR f_gets to get data out of files
+    //f_gets is a wrapper on f_read that does some string formatting for us
+    TCHAR* rres = f_gets((TCHAR*)readBuf, 30, &fil);
+    if(rres != 0) {
+  	PRINT_MESG_DBG("Read string from 'test.txt' contents: %s\r\n", readBuf);
+    } else {
+  	PRINT_MESG_DBG("f_gets error (%i)\r\n", fres);
+    }
+
+    //Be a tidy kiwi - don't forget to close your file!
+    f_close(&fil);
+
+    //Now let's try and write a file "write.txt"
+    fres = f_open(&fil, "write.txt", FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
+    if(fres == FR_OK) {
+  	PRINT_MESG_DBG("I was able to open 'write.txt' for writing\r\n");
+    } else {
+  	PRINT_MESG_DBG("f_open error (%i)\r\n", fres);
+    }
+
+    //Copy in a string
+    strncpy((char*)readBuf, "a new file is made!", 19);
+    UINT bytesWrote;
+    fres = f_write(&fil, readBuf, 19, &bytesWrote);
+    if(fres == FR_OK) {
+  	PRINT_MESG_DBG("Wrote %i bytes to 'write.txt'!\r\n", bytesWrote);
+    } else {
+  	PRINT_MESG_DBG("f_write error (%i)\r\n");
+    }
+
+    //Be a tidy kiwi - don't forget to close your file!
+    f_close(&fil);
+
+    //We're done, so de-mount the drive
+    f_mount(NULL, "", 0);
+
   /* USER CODE END 2 */
 
   /* Init code for STM32_WPAN */
-  MX_APPE_Init();
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 	while(1)
