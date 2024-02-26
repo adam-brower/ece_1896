f8e2b327be8550513983523be7742ab3a6d7ce56
JJ Oswald <jjo50@pitt.edu>
2024-02-26 00:18:18 -0500

    some sd cleanup

--- embedded/brain/Core/Src/main.c
+++ embedded/brain/Core/Src/main.c
@@ -1,14 +1,12 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
- * @file    main.c
- * @author  MCD Application Team
- * @brief   BLE application with BLE core
- *
+  * @file           : main.c
+  * @brief          : Main program body
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2019-2021 STMicroelectronics.
+  * Copyright (c) 2024 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -16,36 +14,15 @@
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
-  @verbatim
-  ==============================================================================
-                    ##### IMPORTANT NOTE #####
-  ==============================================================================
-
-  This application requests having the stm32wb5x_BLE_Stack_fw.bin binary
-  flashed on the Wireless Coprocessor.
-  If it is not the case, you need to use STM32CubeProgrammer to load the appropriate
-  binary.
-
-  All available binaries are located under following directory:
-  /Projects/STM32_Copro_Wireless_Binaries
-
-  Refer to UM2237 to learn how to use/install STM32CubeProgrammer.
-  Refer to /Projects/STM32_Copro_Wireless_Binaries/ReleaseNote.html for the
-  detailed procedure to change the Wireless Coprocessor binary.
-
-  @endverbatim
-  ******************************************************************************
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "Adafruit_RA8875.h"
-
-#include <cstdio>
-#include <string.h>
-#include <stdarg.h> //for va_list var arg functions
-
 #include "app_fatfs.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -64,10 +41,6 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-IPCC_HandleTypeDef hipcc;
-RNG_HandleTypeDef hrng;
-RTC_HandleTypeDef hrtc;
-
 ADC_HandleTypeDef hadc1;
 
 I2C_HandleTypeDef hi2c3;
@@ -101,27 +74,10 @@ static void MX_TIM1_Init(void);
 static void MX_RF_Init(void);
 /* USER CODE BEGIN PFP */
 
-static void MX_IPCC_Init(void);
-
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-int _write(int file, char *ptr, int len)
-{
-  (void)file;
-  int DataIdx;
-  ITM_SendChar(0x42);
-  ITM_SendChar('\n');
-
-  for (DataIdx = 0; DataIdx < len; DataIdx++)
-  {
-    ITM_SendChar(*ptr++);
-  }
-  return len;
-}
-
-uint32_t count;
 
 /* USER CODE END 0 */
 
@@ -139,8 +95,6 @@ int main(void)
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-/* Config code for STM32_WPAN (HSE Tuning must be done before system clock configuration) */
-  MX_APPE_Config();
 
   /* USER CODE BEGIN Init */
 
@@ -151,9 +105,6 @@ int main(void)
 
 /* Configure the peripherals common clocks */
   PeriphCommonClock_Config();
-
-  /* IPCC initialisation */
-  MX_IPCC_Init();
 
   /* USER CODE BEGIN SysInit */
 
@@ -175,126 +126,13 @@ int main(void)
   MX_RF_Init();
   /* USER CODE BEGIN 2 */
 
-  // TODO: cleanup, I moved this up
-  MX_APPE_Init();
-
-/* USER CODE BEGIN 2 */
-
-
-
-  if (MX_FATFS_Init() != APP_OK) {
-    Error_Handler();
-  }
-
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-  /* USER CODE END MX_GPIO_Init_1 */
-
-    /* GPIO Ports Clock Enable */
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-
-
-  HAL_Delay(1000); //a short delay is important to let the SD card settle
-
-  printf("################ Serial Terminal for STM32WB55RG MCU ################%d\n", count);
-
-  printf("################ Read/Write SD Card Demo ################\n");
-
- count = 1;
-
-  printf("this is a count %d\n",count);
-
-    //some variables for FatFs
-    FATFS FatFs; 	//Fatfs handle
-    FIL fil; 		//File handle
-    FRESULT fres; //Result after operations
-
-    int temp = 0;
-
-    //Open the file system
-    fres = f_mount(&FatFs, "", 1); //1=mount now
-    if (fres != FR_OK) {
-  	printf("f_mount error (%i)\r\n", fres);
-  	while(1);
-    }
-
-    //Let's get some statistics from the SD card
-    DWORD free_clusters, free_sectors, total_sectors;
-
-    FATFS* getFreeFs;
-
-    fres = f_getfree("", &free_clusters, &getFreeFs);
-    if (fres != FR_OK) {
-  	printf("f_getfree error (%i)\r\n", fres);
-  	while(1);
-    }
-
-    //Formula comes from ChaN's documentation
-    total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
-    free_sectors = free_clusters * getFreeFs->csize;
-
-    printf("SD card stats:\r\n%10lu KiB total drive space.\r\n%10lu KiB available.\r\n", total_sectors / 2, free_sectors / 2);
-
-    //Now let's try to open file "test.txt"
-    fres = f_open(&fil, "TEST.txt", FA_READ);
-    if (fres != FR_OK) {
-  	printf("f_open error (%i)\r\n");
-  	while(1);
-    }
-    printf("I was able to open 'TEST.txt' for reading!\r\n");
-
-    //Read 100 bytes from "test.txt" on the SD card
-    BYTE readBuf[100];
-
-    //We can either use f_read OR f_gets to get data out of files
-    //f_gets is a wrapper on f_read that does some string formatting for us
-    TCHAR* rres = f_gets((TCHAR*)readBuf, 72, &fil);
-    if(rres != 0) {
-  	printf("Read string from 'test.txt' contents: %s\r\n", readBuf);
-    } else {
-  	printf("f_gets error (%i)\r\n", fres);
-    }
-
-    f_close(&fil);
-
-    //Now let's try and write a file "write.txt"
-    fres = f_open(&fil, "write.txt", FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
-    if(fres == FR_OK) {
-  	printf("I was able to open 'write.txt' for writing\r\n");
-    } else {
-  	printf("f_open error (%i)\r\n", fres);
-    }
-
-    //Copy in a string
-    strncpy((char*)readBuf, "I can write to the SD Card from the embedded code!", 50);
-    UINT bytesWrote;
-    fres = f_write(&fil, readBuf, 50, &bytesWrote);
-    if(fres == FR_OK) {
-  	printf("Wrote %i bytes to 'write.txt'!\r\n", bytesWrote);
-    } else {
-  	printf("f_write error (%i)\r\n");
-    }
-
-    f_close(&fil);
-
-    //We're done, so de-mount the drive
-	f_mount(NULL, "", 0);
-
-    printf("\r\n\r\n################ BLE TX/RX DEMO (Server Side) ################\r\n\r\n");
-
-
-
   /* USER CODE END 2 */
 
-/* Init code for STM32_WPAN */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-	while(1)
-	{
+  while (1)
+  {
     /* USER CODE END WHILE */
-MX_APPE_Process();
 
     /* USER CODE BEGIN 3 */
   }
@@ -309,11 +147,6 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Configure LSE Drive Capability
-  */
-  HAL_PWR_EnableBkUpAccess();
-  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
 
   /** Configure the main internal regulator output voltage
   */
@@ -389,32 +222,6 @@ void PeriphCommonClock_Config(void)
   /* USER CODE BEGIN Smps */
 
   /* USER CODE END Smps */
-}
-
-/**
-  * @brief IPCC Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_IPCC_Init(void)
-{
-
-  /* USER CODE BEGIN IPCC_Init 0 */
-
-  /* USER CODE END IPCC_Init 0 */
-
-  /* USER CODE BEGIN IPCC_Init 1 */
-
-  /* USER CODE END IPCC_Init 1 */
-  hipcc.Instance = IPCC;
-  if (HAL_IPCC_Init(&hipcc) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN IPCC_Init 2 */
-
-  /* USER CODE END IPCC_Init 2 */
-
 }
 
 /**
@@ -544,77 +351,6 @@ static void MX_RF_Init(void)
 
 }
 
-// TODO: determine if needed
-/**
-  * @brief RNG Initialization Function
-  * @param None
-  * @retval None
-  */
-//static void MX_RNG_Init(void)
-//{
-//
-//  /* USER CODE BEGIN RNG_Init 0 */
-//
-//  /* USER CODE END RNG_Init 0 */
-//
-//  /* USER CODE BEGIN RNG_Init 1 */
-//
-//  /* USER CODE END RNG_Init 1 */
-//  hrng.Instance = RNG;
-//  hrng.Init.ClockErrorDetection = RNG_CED_ENABLE;
-//  if (HAL_RNG_Init(&hrng) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  /* USER CODE BEGIN RNG_Init 2 */
-//
-//  /* USER CODE END RNG_Init 2 */
-//
-//}
-
-/**
-  * @brief RTC Initialization Function
-  * @param None
-  * @retval None
-  */
-//static void MX_RTC_Init(void)
-//{
-//
-//  /* USER CODE BEGIN RTC_Init 0 */
-//
-//  /* USER CODE END RTC_Init 0 */
-//
-//  /* USER CODE BEGIN RTC_Init 1 */
-//
-//  /* USER CODE END RTC_Init 1 */
-//
-//  /** Initialize RTC Only
-//  */
-//  hrtc.Instance = RTC;
-//  hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
-//  hrtc.Init.AsynchPrediv = CFG_RTC_ASYNCH_PRESCALER;
-//  hrtc.Init.SynchPrediv = CFG_RTC_SYNCH_PRESCALER;
-//  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
-//  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
-//  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
-//  hrtc.Init.OutPutRemap = RTC_OUTPUT_REMAP_NONE;
-//  if (HAL_RTC_Init(&hrtc) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//
-//  /** Enable the WakeUp
-//  */
-//  if (HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, 0, RTC_WAKEUPCLOCK_RTCCLK_DIV16) != HAL_OK)
-//  {
-//    Error_Handler();
-//  }
-//  /* USER CODE BEGIN RTC_Init 2 */
-//
-//  /* USER CODE END RTC_Init 2 */
-//
-//}
-
 /**
   * @brief SAI1 Initialization Function
   * @param None
@@ -675,7 +411,7 @@ static void MX_SPI1_Init(void)
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_128;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
@@ -708,51 +444,29 @@ static void MX_SPI2_Init(void)
 
   /* USER CODE END SPI2_Init 1 */
   /* SPI2 parameter configuration*/
-	hspi2.Instance = SPI2;
-    hspi2.Init.Mode = SPI_MODE_MASTER;
-    hspi2.Init.Direction = SPI_DIRECTION_2LINES;
-    hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
-    hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
-    hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
-    hspi2.Init.NSS = SPI_NSS_SOFT;
-    hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
-    hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
-    hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
-    hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-    hspi2.Init.CRCPolynomial = 7;
-    hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-    hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
-    if (HAL_SPI_Init(&hspi2) != HAL_OK)
-    {
-	  Error_Handler();
-    }
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 7;
+  hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
   /* USER CODE BEGIN SPI2_Init 2 */
 
   /* USER CODE END SPI2_Init 2 */
 
 }
-
-// TODO: determine if needed
-/**
-  * Enable DMA controller clock
-  */
-//static void MX_DMA_Init(void)
-//{
-//
-//  /* DMA controller clock enable */
-//  __HAL_RCC_DMAMUX1_CLK_ENABLE();
-//  __HAL_RCC_DMA1_CLK_ENABLE();
-//  __HAL_RCC_DMA2_CLK_ENABLE();
-//
-//  /* DMA interrupt init */
-//  /* DMA1_Channel4_IRQn interrupt configuration */
-//  HAL_NVIC_SetPriority(DMA1_Channel4_IRQn, 15, 0);
-//  HAL_NVIC_EnableIRQ(DMA1_Channel4_IRQn);
-//  /* DMA2_Channel4_IRQn interrupt configuration */
-//  HAL_NVIC_SetPriority(DMA2_Channel4_IRQn, 15, 0);
-//  HAL_NVIC_EnableIRQ(DMA2_Channel4_IRQn);
-//
-//}
 
 /**
   * @brief TIM1 Initialization Function
@@ -992,11 +706,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-/**
-  * @brief SPI2 Initialization Function
-  * @param None
-  * @retval None
-  */
 
 /* USER CODE END 4 */
 
@@ -1008,7 +717,10 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-
+  __disable_irq();
+  while (1)
+  {
+  }
   /* USER CODE END Error_Handler_Debug */
 }
 
