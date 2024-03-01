9cbddb2050a2166b1beca683ddf89d5ca2af4be7
JJ Oswald <jjo50@pitt.edu>
2024-03-01 15:59:38 -0500

    added lvgl library

--- embedded/README.md
+++ embedded/README.md
@@ -2,3 +2,7 @@
 
 * Uses STM32CubeWB-1.13.3 Firmware
 * Open Brain Project to build
+
+# LVGL
+
+* Uses lvgl v9.0.0
--- embedded/brain/Core/Src/main.c
+++ embedded/brain/Core/Src/main.c
@@ -122,6 +122,101 @@ int _write(int file, char *ptr, int len)
 
 uint32_t count;
 
+
+// ----------------------------------------------------
+// NOTE:
+// lvgl driver port to stm32wb
+
+#include "stm32wbxx_hal.h"
+#include "lvgl/lvgl.h"
+
+//...
+
+lv_display_t *my_disp;
+volatile int my_disp_bus_busy = 0;
+
+//...
+
+/* DMA transfer ready callback */
+static void my_lcd_color_transfer_ready_cb(SPI_HandleTypeDef *hspi)
+{
+        /* CS high */
+        HAL_GPIO_WritePin(DIS_CS_GPIO_Port, DIS_CS_Pin, GPIO_PIN_SET);
+        my_disp_bus_busy = 0;
+        lv_display_flush_ready(my_disp);
+}
+
+/* Initialize LCD I/O bus, reset LCD */
+static int32_t my_lcd_io_init(void)
+{
+        /* Register SPI Tx Complete Callback */
+        HAL_SPI_RegisterCallback(&hspi1, HAL_SPI_TX_COMPLETE_CB_ID, stm32_lcd_color_transfer_ready_cb);
+
+        /* reset LCD */
+        HAL_GPIO_WritePin(LCD_RESET_GPIO_Port, LCD_RESET_Pin, GPIO_PIN_RESET);
+        HAL_Delay(100);
+        HAL_GPIO_WritePin(LCD_RESET_GPIO_Port, LCD_RESET_Pin, GPIO_PIN_SET);
+        HAL_Delay(100);
+
+        HAL_GPIO_WritePin(DIS_CS_GPIO_Port, DIS_CS_Pin, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(LCD_DCX_GPIO_Port, LCD_DCX_Pin, GPIO_PIN_SET);
+
+        return HAL_OK;
+}
+
+///* Send short command to the LCD. This function shall wait until the transaction finishes. */
+//static void my_lcd_send_cmd(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, const uint8_t *param, size_t param_size)
+//{
+//        LV_UNUSED(disp);
+//        /* Set the SPI in 8-bit mode */
+//        hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+//        HAL_SPI_Init(&hspi1);
+//        /* DCX low (command) */
+//        HAL_GPIO_WritePin(LCD_DCX_GPIO_Port, LCD_DCX_Pin, GPIO_PIN_RESET);
+//        /* CS low */
+//        HAL_GPIO_WritePin(DIS_CS_GPIO_Port, DIS_CS_Pin, GPIO_PIN_RESET);
+//        /* send command */
+//        if (HAL_SPI_Transmit(&hspi1, cmd, cmd_size, BUS_SPI1_POLL_TIMEOUT) == HAL_OK) {
+//                /* DCX high (data) */
+//                HAL_GPIO_WritePin(LCD_DCX_GPIO_Port, LCD_DCX_Pin, GPIO_PIN_SET);
+//                /* for short data blocks we use polling transfer */
+//                HAL_SPI_Transmit(&hspi1, (uint8_t *)param, (uint16_t)param_size, BUS_SPI1_POLL_TIMEOUT);
+//                /* CS high */
+//                HAL_GPIO_WritePin(DIS_CS_GPIO_Port, DIS_CS_Pin, GPIO_PIN_SET);
+//        }
+//}
+//
+///* Send large array of pixel data to the LCD. If necessary, this function has to do the byte-swapping. This function can do the transfer in the background. */
+//static void my_lcd_send_color(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, uint8_t *param, size_t param_size)
+//{
+//        LV_UNUSED(disp);
+//        while (my_disp_bus_busy);       /* wait until previous transfer is finished */
+//        /* Set the SPI in 8-bit mode */
+//        hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+//        HAL_SPI_Init(&hspi1);
+//        /* DCX low (command) */
+//        HAL_GPIO_WritePin(LCD_DCX_GPIO_Port, LCD_DCX_Pin, GPIO_PIN_RESET);
+//        /* CS low */
+//        HAL_GPIO_WritePin(DIS_CS_GPIO_Port, DIS_CS_Pin, GPIO_PIN_RESET);
+//        /* send command */
+//        if (HAL_SPI_Transmit(&hspi1, cmd, cmd_size, BUS_SPI1_POLL_TIMEOUT) == HAL_OK) {
+//                /* DCX high (data) */
+//                HAL_GPIO_WritePin(LCD_DCX_GPIO_Port, LCD_DCX_Pin, GPIO_PIN_SET);
+//                /* for color data use DMA transfer */
+//                /* Set the SPI in 16-bit mode to match endianess */
+//                hspi1.Init.DataSize = SPI_DATASIZE_16BIT;
+//                HAL_SPI_Init(&hspi1);
+//                my_disp_bus_busy = 1;
+//                HAL_SPI_Transmit_DMA(&hspi1, param, (uint16_t)param_size / 2);
+//
+//                /* NOTE: CS will be reset in the transfer ready callback */
+//        }
+//}
+
+
+
+// ----------------------------------------------------
+
 /* USER CODE END 0 */
 
 /**
